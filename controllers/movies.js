c4704c325e2b714c7456609b255f16adc5572261
msiller455 <msiller455@gmail.com>
2022-09-30 13:53:24 -0700

    adds validations and defaults to movie model

--- controllers/movies.js
+++ controllers/movies.js
@@ -14,12 +14,18 @@ function create(req, res) {
     req.body.nowShowing = !!req.body.nowShowing
     req.body.cast = req.body.cast.trim()
     if(req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/)
+    for (let key in req.body) {
+        if(req.body[key] === '') delete req.body[key]
+    }
     const movie = new Movie(req.body)
     movie.save(function(err) {
         // if we don't redirect, the new page will be shown
         // with /movies in the address bar
-        if(err) return res.redirect('/movies/new')
-        res.redirect('/movies/new')
+        if(err) {
+            console.log(err)
+            return res.redirect('/movies/new')
+        }
+        res.redirect('/movies')
     })
 }
 
--- models/movie.js
+++ models/movie.js
@@ -2,11 +2,25 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const movieSchema = new Schema({
-    title: String,
-    releaseYear: Number,
-    mpaaRating: String,
+    title: {
+        type: String,
+        required: true
+    },
+    releaseYear: {
+        type: Number,
+        default: function() {
+            return new Date().getFullYear()
+        },
+        min: 1927
+    },
+    mpaaRating: {
+        type: String,
+        enum: ['G', 'PG', 'PG-13', 'R']
+    },
     cast: [String],
-    nowShowing: Boolean 
+    nowShowing: {type: Boolean, default: false} 
+}, {
+    timestamps: true
 })
 
 module.exports = mongoose.model('Movie', movieSchema)
