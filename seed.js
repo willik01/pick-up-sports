8cba77a96a7e97437f4067ecc8729d5d69485248
msiller455 <msiller455@gmail.com>
2022-10-05 14:48:42 -0700

    adds async await syntax

--- seed.js
+++ seed.js
@@ -26,45 +26,63 @@ const data = require('./data')
 const p1 = Movie.deleteMany({})
 const p2 = Performer.deleteMany({})
 
-Promise.all([p1, p2])
-    .then(function(results) {
-        console.log(results)
-        return Performer.create(data.performers)
-    })
-    .then(function(performers) {
-        console.log(performers)
-        return Movie.create(data.movies)
-    })
-    .then(function(movies) {
-        console.log(movies)
-        return Promise.all([
+// Promise.all([p1, p2])
+//     .then(function(results) {
+//         console.log(results)
+//         return Performer.create(data.performers)
+//     })
+//     .then(function(performers) {
+//         console.log(performers)
+//         return Movie.create(data.movies)
+//     })
+//     .then(function(movies) {
+//         console.log(movies)
+//         return Promise.all([
+//             Performer.findOne({name: 'Mark Hamill'}),
+//             Movie.findOne({title: 'Star Wars - A New Hope'})
+//         ])
+//     })
+//     .then(function(results) {4
+//         const mark = results[0]
+//         const starWars = results[1]
+//         starWars.cast.push(mark._id)
+//         return starWars.save()
+//     })
+//     .then(function(result) {
+//         console.log(result)
+//     })
+//     .then(process.exit)
+
+
+// How to seed the database using async awai
+async function seedAsync() {
+    try {
+        await Promise.all([p1, p2])
+        await Performer.create(data.performers)
+        await Movie.create(data.movies)
+        const pAll2Res = await Promise.all([
             Performer.findOne({name: 'Mark Hamill'}),
             Movie.findOne({title: 'Star Wars - A New Hope'})
         ])
-    })
-    .then(function(results) {
-        const mark = results[0]
-        const starWars = results[1]
+        const [ mark, starWars ] = pAll2Res
         starWars.cast.push(mark._id)
-        return starWars.save()
-    })
-    .then(function(result) {
-        console.log(result)
-    })
-    .then(process.exit)
+        await starWars.save()
+        process.exit()
+    } catch(err) {
+        console.log(err)
+        process.exit()
+    }
+}
+
+seedAsync()
 
 
 
+const p = new Promise(function(resolve, reject) {
+    reject('reject')
+})
 
-
-
-
-
-// const p = new Promise(function(resolve, reject) {
-//     setTimeout(function() {
-//         resolve('Hello World!')
-//     }, 2000)
-// })
+console.log(p)
 
 // p.then(function(result) {
 //     console.log(result)
