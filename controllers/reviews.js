5795072ed55ce088bb043cf7de4fa8fff8190ad5
msiller455 <msiller455@gmail.com>
2022-10-06 15:36:50 -0700

    adds reviews refactor

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -6,6 +6,9 @@ module.exports = {
 
 function create(req, res) {
     Movie.findById(req.params.id, function(err, movie) {
+        req.body.user = req.user._id
+        req.body.userName = req.user.name
+        req.body.userAvatar = req.user.avatar
         movie.reviews.push(req.body)
         movie.save(function(err) {
             res.redirect(`/movies/${movie._id}`)
